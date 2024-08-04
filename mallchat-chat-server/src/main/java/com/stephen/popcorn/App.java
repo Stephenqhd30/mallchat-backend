cdc9965930483d0f30b39e6b5209a6a3ab0b8701
Stephenqhd30 <Popcornqhd@gmail.com>
2024-08-04 15:04:45 +0800

    项目搭建和多环境配置

---
+++ mallchat-chat-server/src/main/java/com/stephen/popcorn/App.java
@@ -0,0 +1,14 @@
+package com.stephen.popcorn;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBoot
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
